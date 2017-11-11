c77bf2c0140892fea9d731ef3cc1a6da87d8f6f9
goo32v2 <goo32v2@gmail.com>
2017-11-11 12:35:50 +0300

    init commit

---
+++ app/src/main/java/com/goo32v2/gurpsmate/GurpsMateApp.kt
@@ -0,0 +1,11 @@
+package com.goo32v2.gurpsmate
+
+import android.app.Application
+
+/**
+ * @author Alexander Steblin (goo32v2)
+ * @date 11.11.2017
+ */
+class GurpsMateApp: Application() {
+
+}
---
+++ app/src/main/java/com/goo32v2/gurpsmate/screens/dashboard/DashboardActivity.kt
@@ -0,0 +1,13 @@
+package com.goo32v2.gurpsmate.screens.dashboard
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.goo32v2.gurpsmate.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
